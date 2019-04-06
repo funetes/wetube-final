e68ddd770ad83c16b20fbc5bd35447a36290a8cc
Kim Hwan <kimlove2324@gmail.com>
2019-04-06 20:58:28 +0900

    #2.14 Layouts with Pug

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ import cookieParser from "cookie-parser";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import routes from "./router";
 const app = express();
 
-app.set("view engin", "pug");
+app.set("view engine", "pug");
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
